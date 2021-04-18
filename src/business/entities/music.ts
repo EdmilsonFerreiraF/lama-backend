b6c2d99abe8106558928984830f4eee69055f4b4
EdmilsonFerreiraF <Edmilsonf.o@hotmail.com>
2021-04-18 18:17:03 -0300

    📝 :pencil: [Minor] Some small updates
    Music data entity for type definitions and to format, music model

---
+++ src/business/entities/music.ts
@@ -0,0 +1,9 @@
+export interface Music {
+    id: string,
+    title: string,
+    author: string,
+    date: Date,
+    file: any
+    genre: string[]
+    album: string
+};
---
+++ src/data/model/Music.ts
@@ -0,0 +1,39 @@
+export class Music {
+  constructor(
+    private id: string,
+    private title: string,
+    private author: string,
+    private date: Date,
+    private file: any,
+    private genre: string[],
+    private album: string
+  ) {};
+
+  public getId(): string {
+    return this.id;
+  };
+
+  public getTitle(): string {
+    return this.title;
+  };
+
+  public getAuthor(): string {
+    return this.author;
+  };
+
+  public getFile(): any {
+    return this.file;
+  };
+
+  public getDate(): Date {
+    return this.date;
+  };
+
+  public getGenre(): string[] {
+    return this.genre;
+  };
+
+  public getAlbum(): string {
+    return this.album;
+  };
+};
