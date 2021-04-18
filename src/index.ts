bf8c4a512a2990ea7f44ca823513751502a80e45
EdmilsonFerreiraF <Edmilsonf.o@hotmail.com>
2021-04-18 18:22:21 -0300

    🚀 :rocket: [Add] Create and reading music
    Feature of creating a music with its data and audio file, also getting music file besides music reading for playing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-
 import { userRouter } from './controller/routes/UserRouter';
+import { musicRouter } from './controller/routes/MusicRouter';
+import path from 'path';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.use('/user', userRouter);
+app.use('/music', musicRouter);
 
 app.listen(process.env.DB_PORT || 3003, () => {
     console.log("Servidor rodando na porta 3003");
-});
+})
