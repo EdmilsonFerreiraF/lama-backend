9c64b8ea1c7e40088f472f63de7d98e5cdf8b110
EdmilsonFerreiraF <Edmilsonf.o@hotmail.com>
2021-04-18 17:50:02 -0300

    📝 :pencil: [Minor] Add music controller layer
    Does receive requests, process music files and return responses

---
+++ src/controller/MusicController.ts
@@ -0,0 +1,127 @@
+import { Request, Response } from "express";
+import { MusicBusiness } from "../business/MusicBusiness";
+import { IdGenerator } from "../business/services/idGenerator";
+import { HashGenerator } from "../business/services/hashGenerator";
+import { MusicDatabase } from "../data/MusicDatabase";
+import { TokenGenerator } from "../business/services/tokenGenerator";
+var multer = require('multer')
+import fs from 'fs';
+const getStat = require('util').promisify(fs.stat);
+
+const musicBusiness =
+ new MusicBusiness(new IdGenerator(),
+                  new HashGenerator(),
+                  new MusicDatabase(),
+                  new TokenGenerator()
+                  );
+
+export class MusicController {
+   public async createMusic(req: Request, res: Response) {
+      try {
+         const { title, author, date, genre, album } = req.body;
+         const file = req.file
+         const token = req.headers.authorization;
+
+         const music = {
+            title,
+            author,
+            date,
+            file,
+            genre,
+            album
+         }
+
+         var storage = multer.diskStorage({
+               destination: function (req: Request, file: any, cb: any) {
+               cb(null, 'uploads')
+            },
+            filename: function (req: Request, file: any, cb: any) {
+               cb(null, Date.now() + '-' + file.originalname )
+            }
+         })
+
+         var upload = multer({ storage: storage })
+         
+         // console.log("req", req)
+         console.log("req.file", req.file)
+         console.log("req.body", req.body)
+         console.log("req.body.title", req.body.title)
+         console.log("file", file)
+
+         // console.log("upload", upload)
+
+         const result = await musicBusiness.createMusic(
+            music,
+            token
+         );
+
+         return res.status(200).send(music.file);
+   
+         // res.status(201).send({ message: "Successfully created" });
+      } catch (error) {
+         const { statusCode, message } = error;
+
+         res.status(statusCode || 400).send({ message });
+      };
+   };
+
+   public async getAllMusics(req: Request, res: Response) {
+      try {
+         const token = req.headers.authorization;
+
+         console.log("token - controller", token)
+         
+         const result: any = await musicBusiness.getAllMusics(
+            token
+         );
+
+         res.send(result)
+      } catch (error) {
+         const { statusCode, message } = error;
+         console.log(error.message || error.sqlMessage)
+         res.status(statusCode || 400).send({ message });
+      };
+   }
+   
+   public async getMusicById(req: Request, res: Response) {
+      try {
+         const { id } = req.params;
+         const token = req.headers.authorization;
+
+         console.log("token - controller", token)
+         
+         const result: any = await musicBusiness.getMusicById(
+            id,
+            token
+         );
+
+         res.send(result)
+      } catch (error) {
+         const { statusCode, message } = error;
+         console.log(error.message || error.sqlMessage)
+         res.status(statusCode || 400).send({ message });
+      };
+   };
+   
+   public async getMusicDetailsById(req: Request, res: Response) {
+      try {
+         const { id } = req.params;
+         const token = req.headers.authorization;
+
+         console.log("token - controller", token)
+         
+         const result: any = await musicBusiness.getMusicDetailsById(
+            id,
+            token
+         );
+
+         res.send(result)
+      } catch (error) {
+         const { statusCode, message } = error;
+         console.log(error.message || error.sqlMessage)
+         res.status(statusCode || 400).send({ message });
+      };
+   };
+};
+
+export default new MusicController();
